83770b2605af6dd9fe816646b06194135038be14
yinglei <yinglei@staff.sina.com.cn>
2019-04-19 11:10:38 +0800

    first commit

---
+++ src/SayHello.php
@@ -0,0 +1,15 @@
+<?php
+/***************************************************************************
+ * @Description:
+ * 
+ * @FileName:         SayHello.php
+ * @Author :          yinglei@staff.sina.com.cn
+ * @CreatedTime:      Fri 19 Apr 2019 10:28:44 AM CST
+ ************************************************************************/
+namespace Hello;
+class SayHello
+{
+    public static function world () {
+        return 'Hello, world!';
+    }
+}
